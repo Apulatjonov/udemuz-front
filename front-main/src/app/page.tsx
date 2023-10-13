f6e5aed7e53ca6b2e37933bd2878d281521a9759
Abdulaziz Pulatjonov <apulotjonov@cdti.uz>
2023-10-13 15:49:59 +0500

    [UdemUz-001]-front

---
+++ front-main/src/app/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { Button } from '@nextui-org/react';
+
+export default function Home () {
+  return <Button className='regular-14 font-bold text-xl' color='primary'>Button</Button>;
+}
